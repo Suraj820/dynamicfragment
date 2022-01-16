78aa40ba6975625bba59f7719255e019ae59d141
Suraj820 <king007suraj@gmail.com>
2022-01-16 17:59:02 +0530

    Removed unused code

--- app/src/main/java/com/java/dynamicfragment/MainActivity.kt
+++ app/src/main/java/com/java/dynamicfragment/MainActivity.kt
@@ -8,25 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       /* if (savedInstanceState == null){
-            findViewById<FragmentContainerView>(R.id.fragment_container).let{ frameLayout ->
-                val listFragment = ListFragment()
-                supportFragmentManager.beginTransaction()
-                    .add(frameLayout.id, listFragment).commit()
-            }
-        }*/
     }
-
-    /*override fun onSelected(starSignId: Int) {
-
-        findViewById<FragmentContainerView>(R.id.fragment_container).let { frameLayout->
-            val detail =  DetailFragment.newInstance(starSignId)
-            supportFragmentManager.beginTransaction()
-                .replace(frameLayout.id,detail)
-                .addToBackStack(null)
-                .commit()
-        }
-    }*/
-
 
 }
